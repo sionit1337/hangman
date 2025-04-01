57b226d39ed0577c8a5b1d64142b52b6145c863b
sionit_1337 <128901310+sionit1337@users.noreply.github.com>
2025-04-01 19:44:15 +0300

    update main.py

--- hangman/main.py
+++ hangman/main.py
@@ -1,8 +1,9 @@
-from __future__ import annotations
+from __future__ import annotations # idk
 from random import choice
+from typing import Callable # i love typehinting
 
 
-TRIES: int = 5
+TRIES: int = 5 # you can edit this
 WORDS: list[str] = ["jazz"]
 
 
@@ -15,7 +16,7 @@ except FileNotFoundError as e:
 
 
 WORDS.sort()
-WORDS = list(filter(bool, WORDS))
+WORDS = list(filter(bool, WORDS)) # remove empty lines if present
 
 
 def main():
@@ -23,10 +24,15 @@ def main():
     wrong: int = 0
     opened: dict[str, bool] = {i: False for i in word}
 
-    while  (not all(list(opened.values()))) and (wrong <= TRIES):
-        __WS: str = "".join([(i if opened[i] else '_') for i in word])
+    # checks
+    is_outoftries: Callable[[], bool] = lambda: not (wrong <= TRIES)
+    is_allopened: Callable[[], bool] = lambda: all(list(opened.values()))
+
+    while  not is_allopened() and not is_outoftries():
+        __WS: str = "".join([(i if opened[i] else '_') for i in word]) # word format string
         letters: str = input(f"tries: {TRIES - wrong} \nword: {__WS} \nany ideas? (>=1 letter(s))> ").lower()
-        if letters == "*":
+
+        if letters == "*": # quit
             print("you can't do that!")
             break
 
@@ -35,7 +41,7 @@ def main():
             else: wrong += 1
 
     print(f"the word was: {word}")
-    if all(list(opened.values())): print("you won!")
+    if is_allopened(): print("you won!")
 
 
 if __name__ == "__main__": main()
