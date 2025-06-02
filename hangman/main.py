1122db323b53a091094ce799b3c498b4b80de2d4
sionit_1337 <128901310+sionit1337@users.noreply.github.com>
2025-06-02 16:09:42 +0300

    rewrite everything
    + settings
    + endless mode
    + statistics

--- hangman/main.py
+++ hangman/main.py
@@ -1,47 +1,138 @@
 from __future__ import annotations # idk
 from random import choice
 from typing import Callable # i love typehinting
+import json
 
 
-TRIES: int = 5 # you can edit this
-WORDS: list[str] = ["jazz"]
+with open("config.json") as file:
+    cfg: dict = json.loads(file.read())
 
 
-try:
-    with open("words.txt", "r") as file:
-        WORDS = file.read().split("\n")
+class Statistics:
+    def __init__(self):
+        self.won: int = 0
+        self.lose: int = 0
 
-except FileNotFoundError as e:
-    print(f"no words.txt found in directory \n[{e}]")
+    @property
+    def total(self): return self.won + self.lose
+    
+    @property
+    def winrate(self): return self.won / self.total
+
+    def reset(self):
+        self.won = 0
+        self.lose = 0
 
 
-WORDS.sort()
-WORDS = list(filter(bool, WORDS)) # remove empty lines if present
+class Options:
+    def __init__(self):
+        self.num_tries: int = 0
+        self.words: list[str] = []
+        self.endless: bool = False
+
+    def load_cfg(self, cfg: dict):
+        num_tries: int = cfg.get("num_tries", 5)
+        endless: bool = cfg.get("endless", False)
+
+        with open(cfg.get("words_file", "words.txt"), "r+") as __F:
+            words: list[str] = __F.read().split("\n")
+            if not any(words): words = ["jazz"]
+            words.sort()
+            words = list(filter(bool, words))
+
+        self.endless = endless
+        self.num_tries = num_tries
+        self.words = words
+
+
+class Game:
+    def __init__(self):
+        self.opts: Options = Options()
+        self.stats: Statistics = Statistics()
+
+        self.wrong: int = 0
+        self.word: str = ""
+        self.opened: dict[str, bool] = {}
+
+        self.reset_opened()
+        self.opts.load_cfg(cfg)
+
+    @property
+    def all_opened(self): return all(list(self.opened.values()))
+
+    @property
+    def out_of_tries(self): return self.wrong > self.opts.num_tries
+
+    @property
+    def complete(self): return (not self.out_of_tries) and (self.all_opened)
+
+    @property
+    def format_string(self): return ''.join(
+            [
+                (i if self.opened[i] else '_') 
+                for i in self.word
+            ]
+        )
+
+    def reset_opened(self):
+        self.opened = {
+            i: False for i in self.word
+        }
+        self.wrong = 0
+
+    def set_word(self, new: str):
+        self.word = new
+        self.reset_opened()
+
+    def try_letters(self, letters: str):
+        if self.out_of_tries: return
+
+        for i in letters:
+            if i in self.opened: self.opened[i] = True
+            else: self.wrong += 1
+
+
+class Hangman(Game):
+    def game(self):
+        self.set_word(choice(self.opts.words))
+
+        while (not self.all_opened) and (not self.out_of_tries):
+            letters = input(
+                f"word: {self.format_string} \n"
+                f"tries left: {self.opts.num_tries - self.wrong} \n"
+                "any ideas? (>=1 letters) > "
+            )
+            
+            self.try_letters(letters)
+
+        print(f"word was: {self.word}")
+        if self.complete: self.stats.won += 1
+        else: self.stats.lose += 1
+
+
+    def start_one(self):
+        self.game()
+        if self.complete: print("you won!")
+
+    def start_endless(self):
+        while True:
+            self.game()
+            print(
+                    "stats \n"
+                    f"games: {self.stats.total} \n"
+                    f"won: {self.stats.won} \n"
+                    f"lose: {self.stats.lose} \n"
+                    f"winrate: {self.stats.winrate}"
+                )
+
+    def start(self):
+        if self.opts.endless: self.start_endless()
+        else: self.start_one()
 
 
 def main():
-    word: str = choice(WORDS).lower()
-    wrong: int = 0
-    opened: dict[str, bool] = {i: False for i in word}
-
-    # checks
-    is_outoftries: Callable[[], bool] = lambda: not (wrong <= TRIES)
-    is_allopened: Callable[[], bool] = lambda: all(list(opened.values()))
-
-    while  not is_allopened() and not is_outoftries():
-        __WS: str = "".join([(i if opened[i] else '_') for i in word]) # word format string
-        letters: str = input(f"tries: {TRIES - wrong} \nword: {__WS} \nany ideas? (>=1 letter(s))> ").lower()
-
-        if letters == "*": # quit
-            print("you can't do that!")
-            break
-
-        for i in letters:
-            if i in word: opened[i] = True
-            else: wrong += 1
-
-    print(f"the word was: {word}")
-    if is_allopened(): print("you won!")
+    h = Hangman()
+    h.start()
 
 
 if __name__ == "__main__": main()
